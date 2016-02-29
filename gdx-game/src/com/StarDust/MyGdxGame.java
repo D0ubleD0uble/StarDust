b1751004e69e56d4c16a3a73e1118bd6962e3635
Cole Hinkel <ColerHinkel@gmail.com>
2016-02-28 22:06:38 -0600

    Add player control

--- gdx-game/src/com/StarDust/MyGdxGame.java
+++ gdx-game/src/com/StarDust/MyGdxGame.java
@@ -8,9 +8,11 @@ import com.StarDust.entity.EntityFactory;
 import com.StarDust.system.Render;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.input.GestureDetector.GestureListener;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
-public class MyGdxGame implements ApplicationListener
+public class MyGdxGame implements ApplicationListener, GestureListener
 {
 	private static Skin uiSkin;
 	//StageManager stageManager;
@@ -56,5 +58,53 @@ public class MyGdxGame implements ApplicationListener
 	public static Skin getUISkin()
 	{
 		return uiSkin;
+	}
+
+	@Override
+	public boolean fling(float arg0, float arg1, int arg2) {
+		System.out.println("fling");
+		return false;
+	}
+
+	@Override
+	public boolean longPress(float arg0, float arg1) {
+		System.out.println("longpress");
+		return false;
+	}
+
+	@Override
+	public boolean pan(float arg0, float arg1, float arg2, float arg3) {
+		System.out.println("pan");
+		return false;
+	}
+
+	@Override
+	public boolean panStop(float arg0, float arg1, int arg2, int arg3) {
+		System.out.println("panStop");
+		return false;
+	}
+
+	@Override
+	public boolean pinch(Vector2 arg0, Vector2 arg1, Vector2 arg2, Vector2 arg3) {
+		System.out.println("pinch");
+		return false;
+	}
+
+	@Override
+	public boolean tap(float arg0, float arg1, int arg2, int arg3) {
+		System.out.println("tap");
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(float arg0, float arg1, int arg2, int arg3) {
+		System.out.println("touchdown");
+		return false;
+	}
+
+	@Override
+	public boolean zoom(float arg0, float arg1) {
+		System.out.println("zoom");
+		return false;
 	}
 }
